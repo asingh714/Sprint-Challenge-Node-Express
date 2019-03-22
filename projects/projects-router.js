c28d192c5efb3883804fd9a2bcc6c7fee6a5b1d4
Aman <amsingh714@gmail.com>
2019-03-22 12:35:05 -0400

    able to DELETE projects

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -119,4 +119,24 @@ router.put("/:id", (req, res) => {
     });
 });
 
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.get(id)
+    .then(project => {
+      if (!project) {
+        res.status(404).json({
+          error: "The project with the specified ID does not exist."
+        });
+      } else {
+        db.remove(id).then(deleted => {
+          res.status(200).json(project);
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: "The project could not be removed." });
+    });
+});
+
 module.exports = router;
