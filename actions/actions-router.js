e72906ec0facd5f29a5b8e767d409e2f0aed0876
Aman <amsingh714@gmail.com>
2019-03-22 12:37:08 -0400

    able to GET actions

--- actions/actions-router.js
+++ actions/actions-router.js
@@ -4,5 +4,17 @@ const db = require("../data/helpers/actionModel");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  db.get()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "The actions could not be retrieved." });
+    });
+});
+
+
+
 
 module.exports = router;
