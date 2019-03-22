c9a53b1259c97e5373992a0b9e5b35c02b1f844b
Aman <amsingh714@gmail.com>
2019-03-22 12:30:46 -0400

    able to GET projects

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -4,8 +4,15 @@ const db = require("../data/helpers/projectModel");
 
 const router = express.Router();
 
-
-
+router.get("/", (req, res) => {
+  db.get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "The projects could not be retrieved." });
+    });
+});
 
 
 module.exports = router;
