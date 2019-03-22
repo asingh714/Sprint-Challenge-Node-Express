7a3a85c2b245e8516b1180aad9ce8fe25be2ef80
Aman <amsingh714@gmail.com>
2019-03-22 12:31:13 -0400

    able to GET projects with specific ID

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -14,5 +14,26 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.get(id)
+    .then(project => {
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res
+          .status(404)
+          .json({ error: "The project with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The project with the specified ID could not be retrieved."
+      });
+    });
+});
+
+
 
 module.exports = router;
