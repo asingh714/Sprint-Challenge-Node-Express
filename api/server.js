7aa016dddccd5abc9a0eb80339053a28ee77304c
Aman <amsingh714@gmail.com>
2019-03-22 12:26:55 -0400

    confgured server

--- api/server.js
+++ api/server.js
@@ -1,18 +1,5 @@
 const express = require("express");
 
-const configureMiddleware = require("../config/middleware.js");
-const projectsRouter = require("../projects/projectsRouter.js");
-const actionsRouter = require("../actions/actionsRouter.js");
-
-const server = express()
-
-
-configureMiddleware(server);
-
-
-server.use("/projects", projectsRouter);
-server.use("/actions", actionsRouter);
-
-
+const server = express();
 
 module.exports = server;
