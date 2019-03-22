4745d524d0a036c453d78f63847d4fdaf6d49d07
Aman <amsingh714@gmail.com>
2019-03-22 12:32:21 -0400

    able to GET actions of specific project

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -34,6 +34,24 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.get("/:id/actions", (req, res) => {
+  const { id } = req.params;
 
+  db.getProjectActions(id)
+    .then(actions => {
+      if (actions.length > 0) {
+        res.status(200).json(actions);
+      } else {
+        res.status(404).json({
+          error: "The actions for the specified project do not exist."
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The actions of the specified project could not be retrieved."
+      });
+    });
+});
 
 module.exports = router;
